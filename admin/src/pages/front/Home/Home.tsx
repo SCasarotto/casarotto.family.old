98934ab3bc179d01b832d3f2c04a01d6ff3adffc
Stuart Casarotto <stuart.casarotto@38thstreetstudios.com>
2021-10-02 21:01:47 -0500

    commented out TODO on home page

--- admin/src/pages/front/Home/Home.tsx
+++ admin/src/pages/front/Home/Home.tsx
@@ -7,9 +7,9 @@ export const Home: React.FC = () => {
     <PanelWrapper>
       <Panel>
         <h1 style={{ textAlign: 'center' }}>Welcome</h1>
-        <p style={{ textAlign: 'center' }}>
+        {/* <p style={{ textAlign: 'center' }}>
           TODO: Build out Request Access Form
-        </p>
+        </p> */}
       </Panel>
     </PanelWrapper>
   );
